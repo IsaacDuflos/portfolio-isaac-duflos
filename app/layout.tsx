66d9cbc837377d473dd5d9afe74763cacda58037
Duflos Isaac <isaac_duflos@ens.univ-artois.fr>
2024-12-03 14:35:53 +0100

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import '@/app/ui/global.css';
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import {shareTechMono} from '@/app/ui/fonts';
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,16 +10,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang={"fr"}>
+        <body className={`${shareTechMono.className} antialiased`}>
+            {children}
+        </body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Share_Tech_Mono } from 'next/font/google';
+
+export const shareTechMono = Share_Tech_Mono({
+    weight: '400',
+    subsets: ['latin'],
+});
